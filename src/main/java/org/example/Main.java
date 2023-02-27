0a76b6fb44cc90732e68fb01a5071865fee5f0c0
Ella <you@example.com>
2023-02-27 23:19:37 +0300

    change BonusServiceTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
         BonusService service = new BonusService();
-    long  expected = 150;
-    long  actual = service.calculate(long amount, boolean registered);
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        System.out.println(expected + "===" + actual);
 
 
+    }
 }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -29,4 +29,17 @@ public class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndUnderLimit(){
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
 }
